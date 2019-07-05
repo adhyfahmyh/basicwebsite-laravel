21b6b92c739a393f08b03c8dffb034c9e80b2b8f
adhyfa <adhyfafahmy@gmail.com>
2019-07-06 01:53:46 +0700

    success input messages data to the table in database

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Message;
 
 class MessagesController extends Controller
 {
@@ -13,5 +14,15 @@ class MessagesController extends Controller
             'email' => 'required',
             'message' => 'required'
         ]);
+        
+        //create a new message
+        $message = new Message;
+        $message->name = $request->input('name');
+        $message->email = $request->input('email');
+        $message->message = $request->input('message');
+
+        //saved message
+        $message->save();
+        return redirect('/');
     }
 }

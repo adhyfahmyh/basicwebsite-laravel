920d30f2551e39aaf22e32bfaa4ea6ac17c99f51
adhyfa <adhyfafahmy@gmail.com>
2019-07-06 02:43:35 +0700

    make pages controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function getHome(){
+        return view('home');
+    }
+    public function getAbout(){
+        return view('about');
+    }
+    public function getContact(){
+        return view('contact');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'PagesController@getHome');
 
-Route::get('/about', function (){
-    return view('about');
-});
+Route::get('/about', 'PagesController@getAbout');
 
-Route::get('/contact', function(){
-    return view('contact');
-});
-
-Route::get('/signin', function(){
-    return view('signin');
-});
+Route::get('/contact', 'PagesController@getContact');
 
 Route::get('/messages', 'MessagesController@getMessages');
 

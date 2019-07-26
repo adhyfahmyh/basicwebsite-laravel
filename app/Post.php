9a1ade13ea9b25824745dc8f5fed0dd41fbb76e6
adhyfa <adhyfafahmy@gmail.com>
2019-07-26 15:07:01 +0700

    commit

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Table name
+    protected $table = 'posts';
+
+    // Primary Key
+    public $primarykey = 'id';
 }

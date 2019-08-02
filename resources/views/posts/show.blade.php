b0178c321993fafe33e99421bec5947f44266775
adhyfa <adhyfafahmy@gmail.com>
2019-08-03 03:52:33 +0700

    edit post feature

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,12 +1,31 @@
 @extends('layout.app')
 
 @section('content')
-    <h1>{{$post->title}}</h1>
+    <h1>{{$post->title}}</h1><br>
+    <a href="/posts" class="btn btn-default" id="back_post">Back</a>
+    <br><br>
     <div>
         {!!$post->body!!}
+        {!!Form:: open(['action' => ['PostsController@destroy', $post->id], 'method'=>'POST', 'class' => 'pull-right']) !!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete', [''])}}
+        {!!Form:: close()!!}
     </div>
     <hr> 
         <small>Written on {{$post->created_at}}</small>
     <br>
-    <a href="/posts" class="btn btn-danger">Back</a>
+    <div>        
+        <a href="/posts/{{$post->id}}/edit" class="btn btn-default" id="edit_post">Edit Post</a>
+    </div>
+    <style>
+        #back_post{
+            background-color: #b5b5b0;
+            color: white;
+            text-align: left;
+        }
+        #edit_post{
+            background-color: #b5b5b0;
+            color: white;
+        }
+    </style>
 @endsection

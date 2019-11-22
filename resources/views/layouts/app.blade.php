74f5f5d21c9463a8fabceb947571fee6bef57c52
adhyfa <adhyfafahmy@gmail.com>
2019-11-23 04:48:03 +0700

    Reset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{-- <title>{{ config('app.name', 'PLearning') }}</title> --}}
-    <title>I'AM BATMAN</title>
+    <title>{{ config('app.name', 'MyLearning') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>

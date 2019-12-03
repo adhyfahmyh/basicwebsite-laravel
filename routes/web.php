d150feee0bf422c5d741380f2c625ed42bc86e07
adhyfa <adhyfafahmy@gmail.com>
2019-12-04 02:32:20 +0700

    reset mail

--- routes/web.php
+++ routes/web.php
@@ -29,24 +29,14 @@ Route::get('/', 'PagesController@index')->middleware('auth');
 
 // Route::get('/profile', 'ProfileController@index');
 
+Route::post('/contact/submit', 'MessagesController@submit')->middleware('auth');
+
 Route::resource('profile', 'ProfileController')->middleware('auth');
 Route::resource('created-content', 'CreatedContentController')->middleware('auth');
 Route::resource('saved-content', 'SavedContentController')->middleware('auth');
- 
-// Route::resource('profile', 'CreatedContentController');
-// Route::get('profile/createdContent', function () {
-//     return view('user.createdContent');
-// });
-
-// Route::get('/profile/{{ Auth::user()->firstname }}/show', 'ProfileController@show');
-
-Route::post('/contact/submit', 'MessagesController@submit')->middleware('auth');
 
 Route::resource('posts', 'PostsController')->middleware('auth');
 Route::resource('contents', 'ContentsController')->middleware('auth');
-// Route::post('/contents/{$content_id}', 'ContentsController@rating');
-// Route::resource(('contents/$content->id'), 'RatingsController');
-// Route::post(('contents/$content->id/selection'), 'SelectionController@store');
 Route::post('contents/$content->id/rating', ['uses' => 'RatingsController@store', 'as' => 'content.rating']);
 Route::post('contents/$content->id/selection', ['uses' => 'SelectionController@store', 'as' => 'content.selection']);
 Route::post('contents/$content->id/timespent', ['uses' => 'TimespentController@store', 'as' => 'content.timespent']);
@@ -61,11 +51,11 @@ Route::get('/dashboard', 'DashboardController@index')->middleware('auth');
 
 Route::get('/test', 'TestController@index');
 
-Route::any('/search', function() {
-    $search = Input::get ( 'search' );
-    $contents = Content::where ( 'title', 'LIKE', '%' . $search . '%' )->orWhere ( 'tags', 'LIKE', '%' . $search . '%' )->get ();
-    if (count ( $contents ) > 0)
-        return view ( 'welcome' )->withDetails ( $contents )->withQuery ( $contents );
-    else
-        return view ( 'welcome' )->withMessage ( 'Konten yang anda cari tidak ada, silahkan coba kembali!' );
-});
+// Route::any('/search', function() {
+//     $search = Input::get ( 'search' );
+//     $contents = Content::where ( 'title', 'LIKE', '%' . $search . '%' )->orWhere ( 'tags', 'LIKE', '%' . $search . '%' )->get ();
+//     if (count ( $contents ) > 0)
+//         return view ( 'welcome' )->withDetails ( $contents )->withQuery ( $contents );
+//     else
+//         return view ( 'welcome' )->withMessage ( 'Konten yang anda cari tidak ada, silahkan coba kembali!' );
+// });

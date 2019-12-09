c221f1b291e64294c97e3b21bcd063a14b217953
adhyfa <adhyfafahmy@gmail.com>
2019-12-09 20:21:37 +0700

    API FIXXXX

--- app/Http/Controllers/ContentRecommendationController.php
+++ app/Http/Controllers/ContentRecommendationController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 use MyLearning\Contents;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpKernel\Client;
+// use Symfony\Component\HttpKernel\Client;
+// use GuzzleHttp\Exception\GuzzleException;
+// use GuzzleHttp\Client;
 
 class ContentRecommendationController extends Controller
 {
@@ -18,54 +20,55 @@ class ContentRecommendationController extends Controller
      */
     public function index()
     {
-        // $user_id = Auth::user()->id;
-        // $data1 = [
-        //     'id_user' => $user_id,
-        // ];
+        $user_id = Auth::user()->id;
+        $data1 = [
+            'id_user' => $user_id,
+        ];
     
-        // $curl = curl_init();
+        $curl = curl_init();
     
-        // curl_setopt_array($curl, array(
-        //     CURLOPT_URL => "18.140.61.0:8001/recommend",
-        //     CURLOPT_RETURNTRANSFER => true,
-        //     CURLOPT_ENCODING => "",
-        //     CURLOPT_MAXREDIRS => 10,
-        //     CURLOPT_TIMEOUT => 30000,
-        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        //     CURLOPT_CUSTOMREQUEST => "POST",
-        //     CURLOPT_POSTFIELDS => json_encode($data1),
-        //     CURLOPT_HTTPHEADER => array(
-        //         // Set here required headers
-        //         // "x-api-key: KiZTkTO9Ex2ZCOr7xmYRA4bInlJc9kVNrVN2INrc",
-        //         "Content-Type:application/json"
-        //     ),
-        // ));
-    
-        // $response = curl_exec($curl);
-        // $err = curl_error($curl);
-        // $json = array(json_decode($response));
-        // $recommendations = $json["recommendation"];
-
-        // curl_close($curl);
-    
-        // if ($err) {
-        //     return view('contents.content-recommendation',['err'=>$err]);
-        // } else {
-        //     return view('contents.content-recommendation',[
-        //         'recommendations'=>$recommendations,
-        //         'json'=>$json,
-        //         'response'=>$response
-        //         ]);
-        // }
-        // $ids = array(36, 35, 32, 33);
-        // $ids_ordered = implode(',', $ids);
-
-        // $contentRecommendation = Contents::whereIn('id', $ids) 
-        // ->orderByRaw(DB::raw("FIELD(id, $ids_ordered)"))
-        // ->get();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "18.140.190.119:8001/recommend",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30000,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($data1,true),
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type:application/json"
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        $json = json_decode($response,true);
         
-        $contentRecommendation = Contents::orderBy('rating', 'DESC')->get();
-        return view('contents.content-recommendation',['contentRecommendation'=>$contentRecommendation,]);
+        $json_dump = json_decode($response,true);
+        $recommendations = array($json["recomendation"]);
+        curl_close($curl);
+        // dump($json ["recommendation"] [0]);
+        $container =[];
+        
+        // foreach($json ["recommendation"] as $value){
+        //     $container[]=json_decode($value, true);
+        // }
+        // dump($container);
+        
+
+        if ($err) {
+            return view('contents.content-recommendation',['err'=>$err]);
+        } else {
+            return view('contents.content-recommendation',[
+                'response'=>$response,
+                'json'=>$json["recommendation"],
+                'recommendations'=>$recommendations,
+                'json_dump'=>$json_dump
+                ]);
+        }
+        
+        // $contentRecommendation = Contents::orderBy('rating', 'DESC')->get();
+        // return view('contents.content-recommendation',['contentRecommendation'=>$contentRecommendation,]);
     }
 
     /**
--- resources/views/contents/content-recommendation.blade.php
+++ resources/views/contents/content-recommendation.blade.php
@@ -5,46 +5,46 @@
         <h2 class="text-center">Rekomendasi Konten Pembelajaran</h2>
     </div>
     <hr>
-    @if (count($contentRecommendation)>0)
+    @if (count($json)>0)
         <ol class="steps">
-            @foreach ($contentRecommendation as $recommendation)
+            @foreach ($json as $recommendation)
                 <li>
                     <div class="card mb-3">
                         <div class="row no-gutters">
                             <div class="col-md-4">
-                            @if (empty($recommendation->content_img))
-                                <img src="https://www.litmos.com/wp-content/uploads/2016/06/blog-eLearning-templates.png" alt="post_image" class="card-img-top" height="250">
-                            @else
-                                <img src="{{ url('/data_file/images/'.$recommendation->content_img) }}" alt="post_image" class="card-img-top" height="250">
-                            @endif
+                                @if (empty($recommendation['content_img']))
+                                    <img src="https://www.litmos.com/wp-content/uploads/2016/06/blog-eLearning-templates.png" alt="post_image" class="card-img-top" height="250">
+                                @else
+                                    <img src="{!! url('/data_file/images/'.$recommendation['content_img']) !!}" alt="post_image" class="card-img-top" height="250">
+                                @endif
                             </div>
                             <div class="col-md-8">
-                            <div class="card-body">
-                                <h3 class="card-title">{{ $recommendation->title }}</h3><hr>
-                                @if (empty($recommendation->description))
-                                    <p class="card-text"><small>Tidak ada deskripsi</small></p>
-                                @else
-                                    <p class="card-text"><small>{{$recommendation->description}}</small></p>
-                                @endif
-                                @if (empty($recommendation->rating))
-                                    <p class="mb-0">
-                                        <img src="data:image/svg+xml;utf8;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pgo8IS0tIEdlbmVyYXRvcjogQWRvYmUgSWxsdXN0cmF0b3IgMTkuMC4wLCBTVkcgRXhwb3J0IFBsdWctSW4gLiBTVkcgVmVyc2lvbjogNi4wMCBCdWlsZCAwKSAgLS0+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiBpZD0iQ2FwYV8xIiB4PSIwcHgiIHk9IjBweCIgdmlld0JveD0iMCAwIDUzLjg2NyA1My44NjciIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDUzLjg2NyA1My44Njc7IiB4bWw6c3BhY2U9InByZXNlcnZlIiB3aWR0aD0iNTEycHgiIGhlaWdodD0iNTEycHgiPgo8cG9seWdvbiBzdHlsZT0iZmlsbDojRUZDRTRBOyIgcG9pbnRzPSIyNi45MzQsMS4zMTggMzUuMjU2LDE4LjE4MiA1My44NjcsMjAuODg3IDQwLjQsMzQuMDEzIDQzLjU3OSw1Mi41NDkgMjYuOTM0LDQzLjc5OCAgIDEwLjI4OCw1Mi41NDkgMTMuNDY3LDM0LjAxMyAwLDIwLjg4NyAxOC42MTEsMTguMTgyICIvPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8L3N2Zz4K" height="23"/>
-                                        <small>belum ada rating</small>
-                                    </p>
-                                @else
-                                    <p class="mb-0">
-                                        <img src="data:image/svg+xml;utf8;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pgo8IS0tIEdlbmVyYXRvcjogQWRvYmUgSWxsdXN0cmF0b3IgMTkuMC4wLCBTVkcgRXhwb3J0IFBsdWctSW4gLiBTVkcgVmVyc2lvbjogNi4wMCBCdWlsZCAwKSAgLS0+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiBpZD0iQ2FwYV8xIiB4PSIwcHgiIHk9IjBweCIgdmlld0JveD0iMCAwIDUzLjg2NyA1My44NjciIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDUzLjg2NyA1My44Njc7IiB4bWw6c3BhY2U9InByZXNlcnZlIiB3aWR0aD0iNTEycHgiIGhlaWdodD0iNTEycHgiPgo8cG9seWdvbiBzdHlsZT0iZmlsbDojRUZDRTRBOyIgcG9pbnRzPSIyNi45MzQsMS4zMTggMzUuMjU2LDE4LjE4MiA1My44NjcsMjAuODg3IDQwLjQsMzQuMDEzIDQzLjU3OSw1Mi41NDkgMjYuOTM0LDQzLjc5OCAgIDEwLjI4OCw1Mi41NDkgMTMuNDY3LDM0LjAxMyAwLDIwLjg4NyAxOC42MTEsMTguMTgyICIvPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8L3N2Zz4K" height="23"/>
-                                        {{ $recommendation->rating }}
-                                        <small>/10</small>
-                                    </p>
-                                @endif
-                            </div>
-                            <div class="card-footer" style="background:none; border:none;">
-                                    <a href="/contents/{{$recommendation->id}}" class="btn btn-success stretched-link col-md-12">Pelajari</a>
+                                <div class="card-body">
+                                    <h3 class="card-title">{{ $recommendation['title'] }}</h3><hr>
+                                    @if (empty($recommendation['description']))
+                                        <p class="card-text"><small>Tidak ada deskripsi</small></p>
+                                    @else
+                                        <p class="card-text"><small>{{$recommendation['description']}}</small></p>
+                                    @endif
+                                    @if (empty($recommendation['rating']))
+                                        <p class="mb-0">
+                                            <img src="data:image/svg+xml;utf8;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pgo8IS0tIEdlbmVyYXRvcjogQWRvYmUgSWxsdXN0cmF0b3IgMTkuMC4wLCBTVkcgRXhwb3J0IFBsdWctSW4gLiBTVkcgVmVyc2lvbjogNi4wMCBCdWlsZCAwKSAgLS0+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiBpZD0iQ2FwYV8xIiB4PSIwcHgiIHk9IjBweCIgdmlld0JveD0iMCAwIDUzLjg2NyA1My44NjciIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDUzLjg2NyA1My44Njc7IiB4bWw6c3BhY2U9InByZXNlcnZlIiB3aWR0aD0iNTEycHgiIGhlaWdodD0iNTEycHgiPgo8cG9seWdvbiBzdHlsZT0iZmlsbDojRUZDRTRBOyIgcG9pbnRzPSIyNi45MzQsMS4zMTggMzUuMjU2LDE4LjE4MiA1My44NjcsMjAuODg3IDQwLjQsMzQuMDEzIDQzLjU3OSw1Mi41NDkgMjYuOTM0LDQzLjc5OCAgIDEwLjI4OCw1Mi41NDkgMTMuNDY3LDM0LjAxMyAwLDIwLjg4NyAxOC42MTEsMTguMTgyICIvPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8L3N2Zz4K" height="23"/>
+                                            <small>belum ada rating</small>
+                                        </p>
+                                    @else
+                                        <p class="mb-0">
+                                            <img src="data:image/svg+xml;utf8;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iaXNvLTg4NTktMSI/Pgo8IS0tIEdlbmVyYXRvcjogQWRvYmUgSWxsdXN0cmF0b3IgMTkuMC4wLCBTVkcgRXhwb3J0IFBsdWctSW4gLiBTVkcgVmVyc2lvbjogNi4wMCBCdWlsZCAwKSAgLS0+CjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgdmVyc2lvbj0iMS4xIiBpZD0iQ2FwYV8xIiB4PSIwcHgiIHk9IjBweCIgdmlld0JveD0iMCAwIDUzLjg2NyA1My44NjciIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDUzLjg2NyA1My44Njc7IiB4bWw6c3BhY2U9InByZXNlcnZlIiB3aWR0aD0iNTEycHgiIGhlaWdodD0iNTEycHgiPgo8cG9seWdvbiBzdHlsZT0iZmlsbDojRUZDRTRBOyIgcG9pbnRzPSIyNi45MzQsMS4zMTggMzUuMjU2LDE4LjE4MiA1My44NjcsMjAuODg3IDQwLjQsMzQuMDEzIDQzLjU3OSw1Mi41NDkgMjYuOTM0LDQzLjc5OCAgIDEwLjI4OCw1Mi41NDkgMTMuNDY3LDM0LjAxMyAwLDIwLjg4NyAxOC42MTEsMTguMTgyICIvPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8Zz4KPC9nPgo8L3N2Zz4K" height="23"/>
+                                            {{ $recommendation['rating'] }}
+                                            <small>/10</small>
+                                        </p>
+                                    @endif
                                 </div>
-                            <div class="card-footer">
-                                <small class="text-muted">Dibuat pada: {{date('d-m-Y', strtotime($recommendation->created_at))}}</small>
-                            </div>
+                                <div class="card-footer" style="background:none; border:none;">
+                                        <a href="/contents/{{$recommendation['id']}}" class="btn btn-success stretched-link col-md-12">Pelajari</a>
+                                    </div>
+                                <div class="card-footer">
+                                    <small class="text-muted">Dibuat pada: {{date('d-m-Y', strtotime($recommendation['created_at']))}}</small>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -54,20 +54,25 @@
     @else
         <h3 class="text-center">Belum ada rekomendasi</h3>
     @endif
-    {{-- {{ $response }}
-    {{ $err }} --}}
-    {{-- <h1>response</h1>
-    {{$response}}
-    <h2>json</h2>
-    {{$json}}
     
-    @foreach($recommendationw as $recommendation)
-        ID: {{ $recommendation['id'] }}
-        Gambar: {{ $recommendation['content_img'] }}
-        Judul: {{ $recommendation['title'] }}
-        Deskripsi: {{ $recommendation['description'] }}
-        Rating: {{ $recommendation['rating'] }}
-
-        Created at: {{ $recommendation['created_at'] }}
+    
+    {{-- @foreach($json as $recommendation)
+    ID: {{ $recommendation['id'] }}
+    Gambar: {{ $recommendation['content_img'] }}
+    Judul: {{ $recommendation['title'] }}
+    Deskripsi: {{ $recommendation['description'] }}
+    Rating: {{ $recommendation['rating'] }}
+        
+    Created at: {{ $recommendation['created_at'] }}
     @endforeach --}}
+    
+    {{-- <h2>Response</h2>
+    {{$response}}
+    <b>tipe data: {{gettype($response)}}</b>
+    <h2>JSON</h2>
+    {{ $json}}//ini coba
+    {{dump($json)}}
+    <b>tipe data: {{gettype($json)}}</b>
+    <h2>Recommendation</h2>
+    {{ $recommendations }} --}}
 @endsection
